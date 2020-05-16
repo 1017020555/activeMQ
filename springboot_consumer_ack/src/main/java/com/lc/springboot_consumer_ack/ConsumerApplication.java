072137114184b738ee174088d325006c32ee2cc9
lc <1017020555@qq.com>
2020-05-16 17:45:36 +0800

    springboot_ack

---
+++ springboot_consumer_ack/src/main/java/com/lc/springboot_consumer_ack/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.lc.springboot_consumer_ack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 消息消费者启动类
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class,args);
+    }
+
+}
---
+++ springboot_producer_ack/src/main/java/com/lc/springboot_producer_ack/SpringbootProducerAckApplication.java
@@ -0,0 +1,13 @@
+package com.lc.springboot_producer_ack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProducerAckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProducerAckApplication.class, args);
+    }
+
+}
